40cc7b5d79cb13e10d957aa4d4bb569b3add9283
Blaine Booher <bgbooher@gmail.com>
2025-10-28 19:25:39 -0500

    minor update to start script, confirmed working

--- start-pumpkin.sh
+++ start-pumpkin.sh
@@ -2,6 +2,7 @@
 export NVM_DIR="/home/led-dreams/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
+sleep 10 # Wait for network to be ready 
 cd /home/led-dreams/interactive-pumpkin/
 git pull origin master
 npm install
